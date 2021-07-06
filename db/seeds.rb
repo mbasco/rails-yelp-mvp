027743ccd30dca6492e633ac1c1eed2d354d07d9
Michelle Basco <hello@mbasco.com>
2021-07-06 22:02:13 +0900

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create(name: Faker::Restaurant.name , address: Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone_number: Faker::PhoneNumber.cell_phone)
+end
